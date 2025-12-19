e07e372988677dda5173de3e6bf842ac0d36fd66
fufu <roycelang24@gmail.com>
2025-12-19 20:30:14 +0800

    feat: add moderate api

--- src/api/moderate.ts
+++ src/api/moderate.ts
@@ -8,10 +8,17 @@ export interface ModerateResponse {
   update: boolean;
 }
 
+const IS_MOCK = true;
+
 export const moderatePost = (request: {
   postId: number;
   request: ModerateRequest;
 }): Promise<ModerateResponse> => {
+  if (IS_MOCK) {
+    setTimeout(() => {
+      return Promise.resolve({ update: true });
+    }, 1000);
+  }
   return apiClient.post<ModerateResponse, ModerateRequest>(
     `admin/v1/posts/${request.postId}/status`,
     request.request
