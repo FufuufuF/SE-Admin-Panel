bc3250c22a766d53b1a47e8205906154e0f4b723
fufu <roycelang24@gmail.com>
2025-12-22 17:27:52 +0800

    chore: update api base-url

--- src/api/core/config.ts
+++ src/api/core/config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: 'http://172.31.68.19:8080/api/v1', // TODO: 修改为后端接口地址
+  baseUrl: 'http://172.31.68.19:8080/', // TODO: 修改为后端接口地址
   timeout: 10000,
   headers: {},
   withCredentials: true,
